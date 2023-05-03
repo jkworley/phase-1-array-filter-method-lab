7b572d1786a847f070ead217c0fbec9e44510a48
Justin Worley <jkworley@gmail.com>
2023-05-03 17:14:46 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(collection, str) {
+    let driver = collection.filter(element => element.toLowerCase() === str.toLowerCase());
+    return driver;
+}
+
+function fuzzyMatch(collection, str) {
+    let driver = collection.filter(element => element.charAt(0) === str.charAt(0));
+    return driver;
+}
+
+function matchName(collection, str) {
+    let driver = collection.filter(element => element.name === str);
+    return driver;
+}
